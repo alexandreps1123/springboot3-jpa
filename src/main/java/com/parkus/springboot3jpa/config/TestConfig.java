c6393a2eaed1b036c5f4c2e21a0dd451cd9cd2c0
alexandreps1123 <alexandre.ps1123@gmail.com>
2023-01-13 09:40:28 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/parkus/springboot3jpa/config/TestConfig.java
+++ src/main/java/com/parkus/springboot3jpa/config/TestConfig.java
@@ -50,6 +50,15 @@ public class TestConfig implements CommandLineRunner {
         categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
+        p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
         User u1 = new User(null, "Maria Breu", "maria@email.com", "99999999", "12345");
         User u2 = new User(null, "Joaquin Leu", "joaquin@email.com", "977777777", "12345");
 
