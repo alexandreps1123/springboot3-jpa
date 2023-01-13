872e81a366fc0cd7bfd240cfa095e33fd55fd433
alexandreps1123 <alexandre.ps1123@gmail.com>
2023-01-13 09:15:59 -0300

    Category

--- src/main/java/com/parkus/springboot3jpa/config/TestConfig.java
+++ src/main/java/com/parkus/springboot3jpa/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.parkus.springboot3jpa.entities.Category;
 import com.parkus.springboot3jpa.entities.Order;
 import com.parkus.springboot3jpa.entities.User;
 import com.parkus.springboot3jpa.entities.enums.OrderStatus;
+import com.parkus.springboot3jpa.repositories.CategoryRepository;
 import com.parkus.springboot3jpa.repositories.OrderRepository;
 import com.parkus.springboot3jpa.repositories.UserRepository;
 
@@ -24,8 +26,15 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
     public void run(String... args) throws Exception {
+
+        Category cat1 = new Category(null, "Eletronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
 
         User u1 = new User(null, "Maria Breu", "maria@email.com", "99999999", "12345");
         User u2 = new User(null, "Joaquin Leu", "joaquin@email.com", "977777777", "12345");
@@ -36,6 +45,7 @@ public class TestConfig implements CommandLineRunner {
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
         
     }
 
