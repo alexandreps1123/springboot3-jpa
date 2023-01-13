70bb64ef8b6012112225da223de98185efe6386a
alexandreps1123 <alexandre.ps1123@gmail.com>
2023-01-13 13:57:38 -0300

    Exception handling - update

--- src/main/java/com/parkus/springboot3jpa/services/UserService.java
+++ src/main/java/com/parkus/springboot3jpa/services/UserService.java
@@ -13,6 +13,8 @@ import com.parkus.springboot3jpa.repositories.UserRepository;
 import com.parkus.springboot3jpa.services.exceptions.DatabaseException;
 import com.parkus.springboot3jpa.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
     
@@ -47,11 +49,16 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
+        try {
+            User entity = repository.getReferenceById(id);
 
-        updateData(entity, obj);
-        
-        return repository.save(entity);
+            updateData(entity, obj);
+            
+            return repository.save(entity);
+        }
+        catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
