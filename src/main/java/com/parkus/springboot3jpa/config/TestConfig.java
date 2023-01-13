0ea01ef458ac55be69b31a2d3f074f463e9d759e
alexandreps1123 <alexandre.ps1123@gmail.com>
2023-01-13 12:12:45 -0300

    Payment, one-to-one association

--- src/main/java/com/parkus/springboot3jpa/config/TestConfig.java
+++ src/main/java/com/parkus/springboot3jpa/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.parkus.springboot3jpa.entities.Category;
 import com.parkus.springboot3jpa.entities.Order;
 import com.parkus.springboot3jpa.entities.OrderItem;
+import com.parkus.springboot3jpa.entities.Payment;
 import com.parkus.springboot3jpa.entities.Product;
 import com.parkus.springboot3jpa.entities.User;
 import com.parkus.springboot3jpa.entities.enums.OrderStatus;
@@ -79,7 +80,12 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
-		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));	
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+        Payment pay1 = new Payment(null, Instant.parse("2023-01-12T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+
+		orderRepository.save(o1);
         
     }
 
